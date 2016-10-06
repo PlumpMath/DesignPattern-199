4ff14d5f8db070b18dc025739ec15db8144c5945
haya <haya@haya-HP>
2016-10-06 21:20:43 +0900

    main change

--- DesignPattern/src/answer/Main1.java
+++ DesignPattern/src/answer/Main1.java
@@ -1,12 +1,9 @@
 package answer;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Main1 {
 
 	public static void main(String[] args) {
-		List<Entry> list = new ArrayList<Entry>();
+		Entry list = new Cons();
 		for(int i = 0; i < 10; i++){
 			list.add(new Content(String.valueOf(i)));
 		}
